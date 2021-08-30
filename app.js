15fe01d1a5eea490e548623702cb2905d663d155
Lanc33llis <scdelance@gmail.com>
2021-08-30 13:10:14 -0500

    removed sass

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express")
 var path = require("path")
 var cookieParser = require("cookie-parser")
 var logger = require("morgan")
-var sassMiddleware = require("node-sass-middleware")
+// var sassMiddleware = require("node-sass-middleware")
 const { Builder, By, until } = require("selenium-webdriver")
 var chrome = require("selenium-webdriver/chrome")
 const { setIntervalAsync } = require("set-interval-async/dynamic")
@@ -107,14 +107,14 @@ app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true,
-  })
-)
+// app.use(
+//   sassMiddleware({
+//     src: path.join(__dirname, "public"),
+//     dest: path.join(__dirname, "public"),
+//     indentedSyntax: true, // true = .sass and false = .scss
+//     sourceMap: true,
+//   })
+// )
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
